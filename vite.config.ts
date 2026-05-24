7b6b027504d84511527794c6bff4ef7fc8b22842
atakhalo <atakhalo@qq.com>
2026-05-24 21:08:02 +0800

    更换默认端口

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1422,
+    port: 5172,
     strictPort: true,
     host: host || false,
     hmr: host
