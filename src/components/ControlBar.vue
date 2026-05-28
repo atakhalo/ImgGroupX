ec68c3cb0f133eb1670f0ef76210714822a9c4cc
atakhalo <atakhalo@qq.com>
2026-05-28 17:33:00 +0800

    标题按钮加文字

--- src/components/ControlBar.vue
+++ src/components/ControlBar.vue
@@ -78,6 +78,7 @@ function handleCollapseLeaves() {
           <svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M4 6h16M4 12h16M4 18h16" />
           </svg>
+          <span>{{ $t('control.title') }}</span>
         </button>
 
         <!-- 折叠菜单按钮 -->
